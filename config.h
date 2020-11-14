c0fd6261e0f36f4508eb1b56cc85190a90160ca9
Suson Waiba <susonwaiba@gmail.com>
2020-11-14 19:03:14 +0545

    updates while monochrome

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Mono:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "Mono:size=10", "JoyPixels:pixelsize=11:antialias=true:autohint=true", "Font Awesome 5 Free Regular:pixelsize=12:antialias=true:autohint=true", "Font Awesome 5 Free Solid:pixelsize=12:antialias=true:autohint=true" };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
